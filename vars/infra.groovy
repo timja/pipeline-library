513fee90d887d45d046085af962cd7e582c8bc65
Tim Jacomb <timjacomb1+github@gmail.com>
2019-07-30 09:10:41 +0100

    Always use azure settings.xml

--- vars/infra.groovy
+++ vars/infra.groovy
@@ -49,12 +49,12 @@ boolean retrieveMavenSettingsFile(String settingsXml, String jdk = 8) {
             }
         }
         return true
-    } else if (jdk.toInteger() > 7 && isRunningOnJenkinsInfra()) {
+    }
         /* Azure mirror only works for sufficiently new versions of the JDK due to Letsencrypt cert */
         writeFile file: settingsXml, text: libraryResource('settings-azure.xml')
         return true
-    }
-    return false
+
+
 }
 
 /**
